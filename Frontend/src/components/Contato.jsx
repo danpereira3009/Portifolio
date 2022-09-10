e821d4b26a4e21ddbf8a21b88f1640f7e12d3a7d
unknown <danpereira3009@gmail.com>
2022-09-10 11:24:47 -0300

    sessão contato responsivo

--- Frontend/src/components/Contato.jsx
+++ Frontend/src/components/Contato.jsx
@@ -31,32 +31,25 @@ function salvarMensagem() {
 
 return (
     
-<section className='bg-gradient-to-t from-blue-200 to-blue-900 h-screen flex justify-center'>
+<section className='bg-gradient-to-t from-[#646165] to-blue-800 md:flex justify-evenly pb-6'>
 
-    <div>
-
-        <div className="text-white mt-8">
-            <h1 className="text-3xl text-center">Entre em contato</h1>
-            <h1 className="text-lg font-serif mt-4 text-center">
-                Estou sempre interessado em projetos freelance e oportunidade de trabalho CLT / PJ. <br/>
-                Se você tiver interesse em contratar meus serviços não exite em utilizar o formulário abaixo.
-            </h1>
-        </div>
-
-        <div className="mt-8 flex flex-col justify-center items-center">        
-            <div className="flex">
-                <InputForm id="Nome" htmlFor="Nome" text="Nome" className="w-[25vw]" placeholder="Digite seu Nome :)" onChange={(e) => {setNome(e.target.value)}}></InputForm>                    
-                <InputForm id="E-Mail" htmlFor="E-Mail" text="E-mail" className="w-[25vw]" placeholder="Digite seu melhor e-mail :)" onChange={(e) => {setEmail(e.target.value)}}></InputForm>
+            <div className="text-white pt-8 md:w-[40vw] relative md:top-24">
+                <h1 className="md:text-4xl text-2xl text-center">Entre em contato</h1>
+                <h1 className="md:text-2xl text-lg p-6 md:p-0 font-serif mt-4 text-justify ">
+                    Estou sempre interessado em projetos freelance e oportunidade de trabalho CLT / PJ. <br/>
+                    Se você tiver interesse em contratar meus serviços não exite em utilizar o formulário abaixo.
+                </h1>
             </div>
 
-            <InputForm id="Assunto" htmlFor="Assunto" text="Assunto" className="w-[51.5vw]" placeholder="Conte-me o motivo do seu contato :)" onChange={(e) => {setAssunto(e.target.value)}}></InputForm>
-            <TextAreaForm  id="Mensagem" htmlFor="Mensagem" text="Mensagem" className="w-[51.5vw] " placeholder="Deixe sua mensagem :)" onChange={(e) => {setMensagem(e.target.value)}}></TextAreaForm>
-            
-            <ButtonSend text="Enviar" onClick={salvarMensagem}/>
-        </div>
+            <div className="flex flex-col items-center mt-8">
 
+                <InputForm id="Nome" htmlFor="Nome" text="Nome" placeholder="Digite seu Nome :)" onChange={(e) => {setNome(e.target.value)}}></InputForm>                    
+                <InputForm id="E-Mail" htmlFor="E-Mail" text="E-mail" placeholder="Digite seu melhor e-mail :)" onChange={(e) => {setEmail(e.target.value)}}></InputForm>
+                <InputForm id="Assunto" htmlFor="Assunto" text="Assunto" placeholder="Conte-me o motivo do seu contato :)" onChange={(e) => {setAssunto(e.target.value)}}></InputForm>
+                <TextAreaForm  id="Mensagem" htmlFor="Mensagem" text="Mensagem" placeholder="Deixe sua mensagem :)" onChange={(e) => {setMensagem(e.target.value)}}></TextAreaForm>
+                <ButtonSend text="Enviar" onClick={salvarMensagem}/>
 
-    </div>
+            </div>
 
 </section>
 
--- Frontend/src/components/atoms/TextAreaForm.jsx
+++ Frontend/src/components/atoms/TextAreaForm.jsx
@@ -4,13 +4,13 @@ export default class InputForm extends Component {
     render() {
         return (
 
-        <div className="relative p-2 mt-2">
+            <div className={`${this.props.className} relative p-2 mt-2`}>
 
-            <textarea id={this.props.id} placeholder={this.props.placeholder} onChange={this.props.onChange} cols="6" rows="6"
-            className={`${this.props.className} peer placeholder-black focus:pl-2 p-2 pl-24 rounded focus:placeholder-white focus:bg-white bg-gray-400 duration-500`} />
+            <textarea id={this.props.id} placeholder={this.props.placeholder} onChange={this.props.onChange} rows="6"
+            className= "peer placeholder-black pl-[110px] focus:pl-2 p-2 rounded-lg focus:placeholder-white focus:bg-white bg-gray-400 duration-500 md:w-[40vw] w-[60vw]"/>
 
             <label htmlFor={this.props.htmlFor} 
-            className="absolute bg-slate-600 text-white p-2 peer-focus:-top-6 top-0 left-0 duration-500 rounded font-bold">
+            className="absolute bg-slate-600 text-white p-2 peer-focus:-top-6 top-0 left-3 duration-500 rounded-lg font-bold">
                 {this.props.text}
             </label>
         </div>
