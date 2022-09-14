c4d67c3de9314fdd8f8b189391cf8cabd68a2f66
unknown <danpereira3009@gmail.com>
2022-09-14 13:34:24 -0300

    gsap animations

--- Frontend/src/components/organisms/Contato.jsx
+++ Frontend/src/components/organisms/Contato.jsx
@@ -3,6 +3,9 @@ import axios from 'axios'
 import InputWithLabel from "../molecules/InputWithLabel"
 import TextAreaWithLabel from "../molecules/TextAreaWithLabel"
 import ButtonForm from "../atoms/ButtonForm"
+import gsap from "gsap"
+import { useRef, useEffect } from "react"
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 export default () => { 
 
@@ -29,27 +32,45 @@ function salvarMensagem() {
     })
 }
 
+    const el = useRef(null)
+    const q = gsap.utils.selector(el);
+    
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(q(".container_"), {
+            scrollTrigger: {
+                trigger: ".trigger",
+                start: "top center",
+                // toggleActions: "none none none none",
+            },
+            y: "-100%",
+            duration: 3,
+            ease: "back",            
+          });
+        }, []);
+
 return (
     
-<section className='bg-gradient-to-t from-[#646165] to-blue-800 md:flex justify-evenly pb-6 font-light'>
+<section ref={el} className='bg-gradient-to-t from-[#646165] to-blue-800 md:flex justify-evenly pb-6 font-light'>
 
-            <div className="text-white pt-8 md:w-[40vw] relative md:top-24">
-                <h1 className="md:text-4xl text-2xl text-center">Entre em contato</h1>
-                <h1 className="md:text-2xl text-lg p-6 md:p-0 mt-4 text-justify ">
-                    Estou sempre interessado em projetos freelance e oportunidade de trabalho CLT / PJ. <br/>
-                    Se você tiver interesse em contratar meus serviços não exite em utilizar o formulário abaixo.
-                </h1>
-            </div>
+    <div className=" text-white pt-8 md:w-[40vw] relative md:top-24">
+        <h1 className="trigger md:text-4xl text-2xl text-center">Entre em contato</h1>
+        <h1 className="md:text-2xl text-lg p-6 md:p-0 mt-4 text-justify ">
+            Estou sempre interessado em projetos freelance e oportunidade de trabalho CLT / PJ. <br/>
+            Se você tiver interesse em contratar meus serviços não exite em utilizar o formulário abaixo.
+        </h1>
+    </div>
 
-            <div className="flex flex-col items-center mt-8">
+    <div className="container_ flex flex-col items-center relative -bottom-[35em] mb-20">
 
-                <InputWithLabel id="Nome" htmlFor="Nome" text="Nome" placeholder="Digite seu Nome :)" onChange={(e) => {setNome(e.target.value)}}></InputWithLabel>                    
-                <InputWithLabel id="E-Mail" htmlFor="E-Mail" text="E-mail" placeholder="Digite seu melhor e-mail :)" onChange={(e) => {setEmail(e.target.value)}}></InputWithLabel>
-                <InputWithLabel id="Assunto" htmlFor="Assunto" text="Assunto" placeholder="Conte-me o motivo do seu contato :)" onChange={(e) => {setAssunto(e.target.value)}}></InputWithLabel>
-                <TextAreaWithLabel  id="Mensagem" htmlFor="Mensagem" text="Mensagem" placeholder="Deixe sua mensagem :)" onChange={(e) => {setMensagem(e.target.value)}}></TextAreaWithLabel>
-                <ButtonForm text="Enviar" onClick={salvarMensagem}/>
+        <InputWithLabel id="Nome" htmlFor="Nome" text="Nome" placeholder="Digite seu Nome :)" onChange={(e) => {setNome(e.target.value)}}></InputWithLabel>                    
+        <InputWithLabel id="E-Mail" htmlFor="E-Mail" text="E-mail" placeholder="Digite seu melhor e-mail :)" onChange={(e) => {setEmail(e.target.value)}}></InputWithLabel>
+        <InputWithLabel id="Assunto" htmlFor="Assunto" text="Assunto" placeholder="Conte-me o motivo do seu contato :)" onChange={(e) => {setAssunto(e.target.value)}}></InputWithLabel>
+        <TextAreaWithLabel id="Mensagem" htmlFor="Mensagem" text="Mensagem" placeholder="Deixe sua mensagem :)" onChange={(e) => {setMensagem(e.target.value)}}></TextAreaWithLabel>
+        <ButtonForm text="Enviar" onClick={salvarMensagem}/>
 
-            </div>
+    </div>
 
 </section>
 
--- Frontend/src/components/organisms/Saudacao.jsx
+++ Frontend/src/components/organisms/Saudacao.jsx
@@ -1,16 +1,35 @@
 import "../../App.css"
+import gsap from "gsap"
+import { useRef, useEffect } from "react"
 
-export default () =>
+function Saudacao() {
 
-<div className="h-[30em] md:text-left text-white relative top-12 left-12 font-light">
+    const el = useRef()
+    const q = gsap.utils.selector(el);
+    
+    useEffect(() => {
+        gsap.to(q(".box"), {
+            x: -1550,
+            duration: 3,
+          });
+        });
 
-    <div>
-        <h1 className="saudacao md:text-6xl text-3xl">Olá, <br/> Meu nome é <span className="text-red-600 letter">D</span>aniel <br/> web developer</h1>
+    return (
+        
+<section ref={el} className="h-[30em] md:text-left text-white relative top-12 left-[100em] font-light">
+
+    <div className="">
+        <h1 className="box md:text-6xl text-3xl">Olá, Meu nome é <span className="font-bold">D</span>aniel <br/> <i>web developer</i> </h1>
     </div>
 
     <div className="mt-16">
-        <h1 className="md:text-4xl text-3xl">Este é meu portifólio</h1>
-        <h2 className="pt-4 md:text-2xl text-lg md:max-w-[25em] max-w-[15em] text-justify">Uma pequena amostra dos projetos que trabalhei, juntamente com uma equipe incrível formada por pessoas de todo Brasil.</h2>
+        <h1 className="box md:text-4xl text-3xl">Este é meu portifólio</h1>
+        <h2 className="box pt-4 md:text-2xl text-lg md:max-w-[25em] max-w-[15em] text-justify">Uma pequena amostra dos projetos que trabalhei, juntamente com uma equipe incrível formada por pessoas de todo Brasil.</h2>
     </div>
-</div>
 
+</section>
+
+    )
+}
+
+export default Saudacao;
