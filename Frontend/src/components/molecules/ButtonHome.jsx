daf107236ca9672df6f6b784070d38608dadf557
unknown <danpereira3009@gmail.com>
2022-09-15 13:22:16 -0300

    posição do bnt Home

--- Frontend/src/components/molecules/ButtonHome.jsx
+++ Frontend/src/components/molecules/ButtonHome.jsx
@@ -6,12 +6,12 @@ function toHome() {
 
 export default () =>
 
-<div className="absolute left-2 flex gap-4">
+<div className="fixed right-28 gap-4">
     <img src={Home} onClick={toHome} className="peer w-8 h-8 hover:cursor-pointer self-center hover:scale-110" 
     alt="botão para retornar a Home"/>
 
-    <div className="flex items-center opacity-0 peer-hover:opacity-100 duration-300">
-        <div className="border-y-8 border-y-transparent border-r-8 border-r-red-400 h-0 w-0"></div>
-        <div className="w-52 text-center self-center p-2 rounded-md bg-red-400 text-white text-xl ">Retornar a Home</div>
+    <div className="absolute -translate-x-[5.5em] mt-2 items-center opacity-0 peer-hover:opacity-100 duration-300">
+        <div className="border-x-8 border-x-transparent border-b-8 border-b-red-400 translate-x-24 h-0 w-0"></div>
+        <div className="w-52 text-center self-center p-2 rounded-md bg-red-400 text-white text-xl">Retornar a Home</div>
     </div>
 </div>
